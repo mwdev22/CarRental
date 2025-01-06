4539b26334451986530bd6a3f249f311ce67cc75
mwdev22 <maciekwojdyna1@gmail.com>
2025-01-06 14:02:12 +0100

    small name convention change

---
+++ internal/utils/sql.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/mwdev22/CarRental/internal/types"
+)
+
+func BuildBatchQuery(query string, filters []*types.QueryFilter, opts *types.QueryOptions) (string, []interface{}) {
+	args := make([]interface{}, 0)
+	// i use question marks because filters and their count are dynamic
+	// could be 1, 2, 3 etc through loop, but its not necessary complexity i think
+	for _, filter := range filters {
+		query += fmt.Sprintf(" AND %s %s ?", filter.Field, filter.Operator)
+		args = append(args, filter.Value)
+	}
+
+	if opts != nil {
+		query += ` ORDER BY ? ?`
+		query += ` LIMIT ? OFFSET ?`
+		args = append(args, opts.SortField, opts.SortDiretion, opts.Limit, opts.Offset)
+	} else {
+		query += ` ORDER BY id DESC`
+	}
+	return query, args
+}
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"os"
 	"time"
-
-	"github.com/mwdev22/CarRental/internal/types"
 )
 
 func MakeLogger(filename string) (*log.Logger, error) {
@@ -16,25 +14,6 @@ func MakeLogger(filename string) (*log.Logger, error) {
 	}
 	logger := log.New(logFile, "", log.LstdFlags)
 	return logger, nil
-}
-
-func BuildBatchQuery(query string, filters []*types.QueryFilter, opts *types.QueryOptions) (string, []interface{}) {
-	args := make([]interface{}, 0)
-	// i use question marks because filters and their count are dynamic
-	// could be 1, 2, 3 etc through loop, but its not necessary complexity i think
-	for _, filter := range filters {
-		query += fmt.Sprintf(" AND %s %s ?", filter.Field, filter.Operator)
-		args = append(args, filter.Value)
-	}
-
-	if opts != nil {
-		query += ` ORDER BY ? ?`
-		query += ` LIMIT ? OFFSET ?`
-		args = append(args, opts.SortField, opts.SortDiretion, opts.Limit, opts.Offset)
-	} else {
-		query += ` ORDER BY id DESC`
-	}
-	return query, args
 }
 
 func GenerateUniqueString(base string) string {
